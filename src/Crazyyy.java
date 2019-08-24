8f176a0b2117b14ba190f57de38ee4d72235d668
cbinitish <cbinitish@gmail.com>
2019-08-24 11:41:20 -0500

    Initial commit

---
+++ src/Crazyyy.java
@@ -0,0 +1,8 @@
+public class Crazyyy {
+
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            int q = 12;
+            String w = "Nitish";
+        }
+    }
